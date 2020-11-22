860315896740aa0dcabceca33a4fbbac011f66d9
renkataoka <530lotus@gmail.com>
2020-11-22 10:54:44 +0900

    MainActivityにViewの実装を追加。

--- app/src/main/java/com/renkataoka/dubugger/module/main/view/MainActivity.java
+++ app/src/main/java/com/renkataoka/dubugger/module/main/view/MainActivity.java
@@ -2,15 +2,46 @@ package com.renkataoka.dubugger.module.main.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import com.renkataoka.dubugger.R;
+import com.renkataoka.dubugger.module.main.contract.MainContract;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MainContract.View {
+
+    /**
+     * Presenterクラスのオブジェクト。
+     */
+    private MainContract.Presenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        presenter = beginAssembleModules(this);
+    }
+
+    @Override
+    protected void onDestroy() {
+        beginDisassembleModules();
+        super.onDestroy();
+    }
+
+    @Override
+    public void onDisassemble() {
+        presenter = null;
+    }
+
+    @Override
+    public MainContract.Presenter beginAssembleModules(Context context) {
+        return null;
+    }
+
+    @Override
+    public void beginDisassembleModules() {
+        if (presenter != null) {
+            presenter.disassembleModules();
+        }
     }
 }

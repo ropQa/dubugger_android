dc2ec8f414e00b6cd38f178940f214be9f1ce751
renkataoka <530lotus@gmail.com>
2020-11-23 12:13:04 +0900

    MainInteractorにInteractorの実装を追加。

--- app/src/main/java/com/renkataoka/dubugger/module/main/interactor/MainInteractor.java
+++ app/src/main/java/com/renkataoka/dubugger/module/main/interactor/MainInteractor.java
@@ -1,7 +1,33 @@
 package com.renkataoka.dubugger.module.main.interactor;
 
+import com.renkataoka.dubugger.module.main.contract.MainContract;
+import com.renkataoka.viper.InteractorCallback;
+
 /**
  * Main画面のInteractorクラス。
  */
-public class MainInteractor {
+public class MainInteractor implements MainContract.Interactor {
+
+    /**
+     * InteractorCallbackクラスのオブジェクト。
+     */
+    private MainContract.InteractorCallback callback;
+
+    /**
+     * コンストラクタ
+     */
+    public MainInteractor() {
+    }
+
+    @Override
+    public void setInteractorCallback(InteractorCallback callback) {
+        if (callback instanceof MainContract.InteractorCallback) {
+            this.callback = (MainContract.InteractorCallback) callback;
+        }
+    }
+
+    @Override
+    public void onDisassemble() {
+        callback = null;
+    }
 }

50afbfc30adb7bcc4e0298618183b136856a5452
renkataoka <530lotus@gmail.com>
2020-11-24 22:02:09 +0900

    MainRouterにRouterの実装を追加。

--- app/src/main/java/com/renkataoka/dubugger/module/main/router/MainRouter.java
+++ app/src/main/java/com/renkataoka/dubugger/module/main/router/MainRouter.java
@@ -1,7 +1,18 @@
 package com.renkataoka.dubugger.module.main.router;
 
+import android.content.Context;
+
+import com.renkataoka.dubugger.module.main.contract.MainContract;
+
 /**
  * メイン画面のRouterクラス。
  */
-public class MainRouter {
+public class MainRouter implements MainContract.Router {
+    private Context context;
+
+    @Override
+    public void onDisassemble() {
+        //Router側でContextを破棄することで、繋ぐViewとの結合を解除する。
+        context = null;
+    }
 }

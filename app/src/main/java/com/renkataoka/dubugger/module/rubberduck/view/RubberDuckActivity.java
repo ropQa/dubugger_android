46da0111c399c8d5e78567f7ec9b7418c69fb4b3
renkataoka <530lotus@gmail.com>
2020-11-24 21:31:27 +0900

    RubberDuckActivityを作成。

---
+++ app/src/main/java/com/renkataoka/dubugger/module/rubberduck/view/RubberDuckActivity.java
@@ -0,0 +1,16 @@
+package com.renkataoka.dubugger.module.rubberduck.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.renkataoka.dubugger.R;
+
+public class RubberDuckActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rubber_duck);
+    }
+}

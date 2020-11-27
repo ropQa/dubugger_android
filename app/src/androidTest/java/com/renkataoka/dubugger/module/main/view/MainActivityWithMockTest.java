85bf6579f91fb3076e0d9ed97e45c4bf1bafb44d
renkataoka <530lotus@gmail.com>
2020-11-27 23:33:14 +0900

    MainActivity#setToDebugItems()の単体テストを作成。

--- app/src/androidTest/java/com/renkataoka/dubugger/module/main/view/MainActivityWithMockTest.java
+++ app/src/androidTest/java/com/renkataoka/dubugger/module/main/view/MainActivityWithMockTest.java
@@ -4,13 +4,20 @@ import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.lifecycle.Lifecycle;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.platform.app.InstrumentationRegistry;
 
 import com.renkataoka.dubugger.R;
+import com.renkataoka.dubugger.entity.ToDebugItems;
 import com.renkataoka.dubugger.module.main.presenter.MockMainPresenter;
 
 import org.junit.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 import static org.junit.Assert.*;
 
@@ -75,5 +82,77 @@ public class MainActivityWithMockTest {
             button.performClick();
         });
         assertEquals(0, mockPresenter.getCountOnAddButtonClicked());
+    }
+
+    /**
+     * テストデータを3件作り、setToDebugItems()に渡す。
+     * 3件とも正しく表示されるかをテストする。
+     */
+    @Test
+    public void setToDebugItems() {
+        //UIスレッド上で、activityへの参照を得るためのリファレンス。
+        AtomicReference<MainActivityWithMock> atomicReference = new AtomicReference<>();
+
+        // テストデータ生成
+        List<ToDebugItems> toDebugItems = new ArrayList<>();
+        ToDebugItems item;
+        //1件目
+        String string_1 = "Content1";
+        item = new ToDebugItems();
+        item.setContent(string_1);
+        toDebugItems.add(item);
+
+        //2件目
+        String string_2 = "Content2";
+        item = new ToDebugItems();
+        item.setContent(string_2);
+        toDebugItems.add(item);
+
+        //3件目
+        String string_3 = "Content3";
+        item = new ToDebugItems();
+        item.setContent(string_3);
+        toDebugItems.add(item);
+
+        scenario.onActivity(activity -> {
+            activity.setToDebugItems(toDebugItems);
+            atomicReference.set(activity);
+        });
+
+        MainActivityWithMock activity = atomicReference.get();
+        InstrumentationRegistry.getInstrumentation().waitForIdleSync();
+
+        RecyclerView recyclerView = activity.findViewById(R.id.recyclerViewToDebugList);
+        assertEquals(3, recyclerView.getChildCount());
+
+        //表示の確認
+        RecyclerView.ViewHolder viewHolder;
+        ToDebugItemViewHolder toDebugItemViewHolder;
+
+        int position = 0;
+
+        //1件目
+        viewHolder = recyclerView.findViewHolderForAdapterPosition(position);
+        assertTrue(viewHolder instanceof ToDebugItemViewHolder);
+        toDebugItemViewHolder = (ToDebugItemViewHolder) viewHolder;
+        assertEquals(string_1, toDebugItemViewHolder.itemContentView.getText().toString());
+        position++;
+
+        //2件目
+        viewHolder = recyclerView.findViewHolderForAdapterPosition(position);
+        assertTrue(viewHolder instanceof ToDebugItemViewHolder);
+        toDebugItemViewHolder = (ToDebugItemViewHolder) viewHolder;
+        assertEquals(string_2, toDebugItemViewHolder.itemContentView.getText().toString());
+        position++;
+
+        //3件目
+        viewHolder = recyclerView.findViewHolderForAdapterPosition(position);
+        assertTrue(viewHolder instanceof ToDebugItemViewHolder);
+        toDebugItemViewHolder = (ToDebugItemViewHolder) viewHolder;
+        assertEquals(string_3, toDebugItemViewHolder.itemContentView.getText().toString());
+        position++;
+
+        //4件目は存在しない。
+        assertNull(recyclerView.findViewHolderForAdapterPosition(position));
     }
 }

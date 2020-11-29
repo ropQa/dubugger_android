a859cbe97b5bcb66a3a03fdb27718aa24a69a343
renkataoka <530lotus@gmail.com>
2020-11-29 17:57:24 +0900

    RubberDuckInteractorにInteractorの実装を追加。

--- app/src/main/java/com/renkataoka/dubugger/module/rubberduck/RubberDuckInteractor.java
+++ app/src/main/java/com/renkataoka/dubugger/module/rubberduck/RubberDuckInteractor.java
@@ -1,4 +1,28 @@
 package com.renkataoka.dubugger.module.rubberduck;
 
-public class RubberDuckInteractor {
+import com.renkataoka.dubugger.module.main.contract.MainContract;
+import com.renkataoka.dubugger.module.rubberduck.contract.RubberDuckContract;
+import com.renkataoka.viper.InteractorCallback;
+
+/**
+ * RubberDuck画面のInteractorクラス。
+ */
+public class RubberDuckInteractor implements RubberDuckContract.Interactor {
+
+    /**
+     * InteractorCallbackクラスのオブジェクト。
+     */
+    private MainContract.InteractorCallback callback;
+
+    @Override
+    public void setInteractorCallback(InteractorCallback callback) {
+        if (callback instanceof RubberDuckContract.InteractorCallback) {
+            this.callback = (MainContract.InteractorCallback) callback;
+        }
+    }
+
+    @Override
+    public void onDisassemble() {
+        callback = null;
+    }
 }
